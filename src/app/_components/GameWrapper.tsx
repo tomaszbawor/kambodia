202da6cb93543c445aa425b175f1016c3a1dade2
Tomasz Bawor <bawortomasz@gmail.com>
2023-09-01 22:30:34 +0200

    Add button component

---
+++ src/app/_components/GameWrapper.tsx
@@ -0,0 +1,12 @@
+import {FC, PropsWithChildren} from "react";
+
+
+export const GameWrapper: React.FC<PropsWithChildren> = ({children}) => {
+    return (
+        <main className="w-screen h-screen flex items-center justify-center">
+            <div className={'h-[768px] max-h-[768px] max-w-[1024px] w-[1024px] bg-gray-400 p-4 '}>
+                {children}
+            </div>
+        </main>
+    )
+}
